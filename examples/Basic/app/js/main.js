4d3b45339d0ce7ecaca61727bc61638776022a1c
Bram Bogaerts <bram@superposition.cc>
2019-02-15 15:39:43 +0100

    Add gyroscope example

--- examples/Basic/app/js/main.js
+++ examples/Basic/app/js/main.js
@@ -1,3 +1,12 @@
+const compass1 = document.querySelector('.compass1');
+const compass2 = document.querySelector('.compass2');
+
 window.addEventListener('deviceorientation', function(orientation) {
-	console.log(orientation.alpha);
+	const x = orientation.beta;
+	const y = orientation.gamma;
+	const z = orientation.alpha;
+
+	compass1.style.transform = 'translate(-50%, -50%) rotateZ(' + z + 'deg) rotateY(' + y + 'deg) rotateX(' + x + 'deg)';
+
+	compass2.style.transform = 'translate(-50%, -50%) rotateZ(' + -z + 'deg) rotateY(' + -y + 'deg) rotateX(' + -x + 'deg)';
 });
