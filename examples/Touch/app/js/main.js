36968b3a1ee7405f4bc8aa8c5a2a90c97c9bb403
Bram Bogaerts <bram@superposition.cc>
2019-02-22 14:38:28 +0100

    Update Touch example

--- examples/Touch/app/js/main.js
+++ examples/Touch/app/js/main.js
@@ -2,13 +2,32 @@ const circle = document.createElement('div');
 circle.classList.add('circle');
 document.body.appendChild(circle);
 
-document.addEventListener("touchmove", function (e) {
+const label = document.createElement('div');
+label.classList.add('label');
+circle.appendChild(label);
+
+
+if (navigator.userAgent.match('iPhone')) {
+	label.innerHTML = 'iPhone';
+} else if (navigator.userAgent.match('Android')) {
+	label.innerHTML = 'Android';
+} else {
+	label.innerHTML = 'Other';
+}
+
+document.addEventListener('touchstart', function (e) {
+	circle.classList.add('touched');
+});
+
+document.addEventListener('touchend', function (e) {
+	circle.classList.remove('touched');
+});
+
+document.addEventListener('touchmove', function (e) {
 	e.preventDefault();
 
-	console.log(
-		e.touches[0].pageX,
-		e.touches[0].pageY
-	);
+	circle.style.left = e.touches[0].pageX + 'px';
+	circle.style.top = e.touches[0].pageY + 'px';
 }, {
 	passive: false
 });
